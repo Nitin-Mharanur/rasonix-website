a1c57b66656b773619a6723706809604fe937329
Bhavish <bhavish.muneshwar@rasonix.com>
2023-05-25 12:28:51 +0530

    dashboard created

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,18 @@
+import BarChart from "@/components/admin/BarChart";
+import RecentUpdate from "@/components/admin/RecentUpdate";
+import TopCards from "@/components/admin/TopCards";
 import React from "react";
 
 function DashboardPage() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <TopCards />
+      <div className="p-4 grid md:grid-cols-3 grid-cols-1 gap-4">
+        <BarChart />
+        <RecentUpdate />
+      </div>
+    </div>
+  );
 }
 
 export default DashboardPage;
